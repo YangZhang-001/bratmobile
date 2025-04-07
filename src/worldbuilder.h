07433fc2ce828accbec4dcb2cba5180e661b6f44
glafratta <g.lafratta@outlook.com~>
2025-04-07 14:58:23 +0100

    changed worldbuilder threshold for clustering

--- src/worldbuilder.h
+++ src/worldbuilder.h
@@ -33,7 +33,7 @@ class WorldBuilder{
 
     std::vector <BodyFeatures> getFeatures(const CoordinateContainer &, b2Transform, Direction , float, float halfWindowWidth=.1, CLUSTERING clustering=BOX);
 
-    std::vector <BodyFeatures> buildWorld(b2World&,const CoordinateContainer&, b2Transform, Direction,  Disturbance disturbance=Disturbance(), float halfWindowWidth=.15, CLUSTERING clustering=BOX, Task * task=NULL);
+    std::vector <BodyFeatures> buildWorld(b2World&,const CoordinateContainer&, b2Transform, Direction,  Disturbance disturbance=Disturbance(), float halfWindowWidth=.15, CLUSTERING clustering=CLUSTERING::PARTITION, Task * task=NULL);
 
     std::pair <Pointf, Pointf> bounds(Direction, b2Transform t, float boxLength, float halfWindowWidth); //returns bottom and top of bounding box
 
