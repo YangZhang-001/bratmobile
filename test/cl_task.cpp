7eebeb9a4a45b1cc60cbb0a176a04bb1f2adf6ed
glafratta <g.lafratta@outlook.com~>
2025-04-16 16:12:57 +0100

    debug from conf interface

--- test/cl_task.cpp
+++ test/cl_task.cpp
@@ -58,7 +58,7 @@ int main(int argc, char** argv) {
 	Motor_Out controlInterface;
     Configurator configurator(controlGoal);
 	if (argc>2){
-		#define DEBUG atoi(argv[2])
+		configuratorInterface.debugOn=atoi(argv[2]);
 	}
 	configurator.setSimulationStep(.5);
 	as=AffordanceSetter(AffordanceIndex(atoi(argv[1])));
