5d7362877c6c1c21c396d172cf5490293814338c
glafratta <g.lafratta@outlook.com>
2025-04-17 17:12:32 +0100

    no longer adjusting end criterai in track task

--- src/configurator.cpp
+++ src/configurator.cpp
@@ -1024,7 +1024,6 @@ void Configurator::track_task_execution(){
 	if (iteration>1){
 		deltaPose=worldBuilder.wb_bridger.get_transform(currentTask, data2fp, &currentTask.disturbance, worldBuilder.world_objects); //track using obstacle OR dead reckoning
 	}
-	//currentTask.endCriteria.adjust(deltaPose); //adjusting in task so system can be memoryless
 	printf("end criteria: a=%f, d=%f\n", currentTask.endCriteria.angle.get_signed(), currentTask.endCriteria.distance.get_signed());
 	update_graph(transitionSystem, deltaPose, &currentTask, &controlGoal);
 	ended=currentTask.checkEnded(task_sensor, b2Transform_zero, worldBuilder.wb_bridger.get_tracked_disturbance()); //the sensor moves with the robot
