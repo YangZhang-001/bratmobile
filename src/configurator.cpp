afeb6b9fb6ad92b17ccef223a5cd6d1ee7a1a4fd
glafratta <g.lafratta@outlook.com>
2025-04-17 15:27:36 +0100

    not assigning to current vertex

--- src/configurator.cpp
+++ src/configurator.cpp
@@ -540,7 +540,7 @@ void Configurator::run(Configurator * c){
 			c->Spawner();
 			printf("graph size=%i\n", c->transitionSystem.m_vertices.size());
 			c->track_task_execution();
-			c->currentVertex=c->estimate_current_vertex(c->transitionSystem, c->currentTask,c->currentVertex);
+			c->estimate_current_vertex(c->transitionSystem, c->currentTask,c->currentVertex);
 			if (( c->getTask()->change& c->transitionSystem[c->currentVertex].direction!=STOP && c->plan.empty() && c->getIteration()>1)){
 				printf("change goal");
 				c->goal_changer->change_goal(&c->controlGoal);
