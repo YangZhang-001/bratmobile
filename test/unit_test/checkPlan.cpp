7981729f24fab8b5f754c79a938ca896208d108e
glafratta <g.lafratta@outlook.com~>
2025-04-28 15:44:16 +0100

    fixed tests

--- test/unit_test/checkPlan.cpp
+++ test/unit_test/checkPlan.cpp
@@ -39,8 +39,10 @@ int main(int argc, char** argv){
     int ogstep=conf.transitionSystem[conf.currentEdge].step;
     conf.getTask()->action.L=0.5;
     conf.getTask()->action.R=0.5;
-    for (int i=0;i<di.iteration*2; i++){
-
+    int it=di.iteration;
+    for (int i=0;i<it; i++){
+        di.newScanAvail();          
+        conf.data2fp = ci.data2fp;
         conf.track_task_execution();
         conf.estimate_current_vertex(conf.transitionSystem, *conf.getTask());
         conf.getTask()->motorStep--;
@@ -49,8 +51,8 @@ int main(int argc, char** argv){
         if (ch){
             conf.getTask()->motorStep=100; //simulate new step setting because we are in open loop
         }
-        di.newScanAvail();          
-        conf.data2fp = ci.data2fp;}
+
+        }
     // if (argc>4){
     //     di.iteration=steps;
     //     conf.addIteration(steps-conf.getIteration()+1);
