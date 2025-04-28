6fb355e4afa0c1499429570e1d94675b4048ae51
glafratta <g.lafratta@outlook.com~>
2025-04-28 12:17:27 +0100

    trying to fix test cases

--- test/unit_test/closed_loop_ended.cpp
+++ test/unit_test/closed_loop_ended.cpp
@@ -9,10 +9,9 @@ int main(int argc, char ** argv){
     bf_ob.pose.p.x=0.5;
     bf_ob.pose.p.y=0;
     bf_ob.pose.q.Set(0);
-    Disturbance goal(PURSUE, b2Vec2(1.0, 0), 0), obstacle(bf_ob);
+    Disturbance goal(PURSUE, b2Vec2(1.0, 0), 0), obstacle_og(bf_ob), obstacle=obstacle_og;
     AffordanceIndex aff=AffordanceIndex(atoi(argv[5]));
     math::applyAffineTrans(transform, obstacle);
-    //math::applyAffineTrans(transform, goal);
     b2Transform sensor_transform=b2Transform_zero;
     b2PolygonShape sensor= wb.sensor_box(Robot::get_vertices(), sensor_transform, &goal);
     b2AABB aabb;
@@ -20,11 +19,12 @@ int main(int argc, char ** argv){
     b2Vec2 center=aabb.GetCenter(); //for debug
     Direction dir=Direction(atoi(argv[4]));
     obstacle.set_affordance(aff);
+    obstacle_og.set_affordance(aff);
     Task t(obstacle, dir);
     if (aff==PURSUE && dir==DEFAULT){
         t.setEndCriteria(Distance(0.1)); //stop 10 cm before obstacle 
     }
-    bool ended=t.checkEnded(sensor, b2Transform_zero , &obstacle);
+    bool ended=t.checkEnded(sensor, b2Transform_zero , &obstacle_og);
     debug_draw(sensor.m_vertices, obstacle.vertices());
     return !ended;
 
