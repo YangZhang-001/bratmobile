3902bfc63cfcd4e04f085396baf63393d261829e
glafratta <g.lafratta@outlook.com>
2025-04-30 18:19:51 +0100

    not messing around with task endcriteria in the new task to be executed

--- src/configurator.cpp
+++ src/configurator.cpp
@@ -1077,8 +1077,8 @@ Task Configurator::task_to_execute(const std::vector<vertexDescriptor>&p, const 
 	else{
 		t=Task(g[p[0]].Di, g[p[0]].direction, b2Transform_zero, true);
 		//t.disturbance.bf.pose=g[p[0]].start_from_Di();
-		b2Transform end_from_Di=g[p[0]].end_from_Di();
-		t.endCriteria.angle.set(atan2(end_from_Di.p.y, end_from_Di.p.x));
+		//b2Transform end_from_Di=g[p[0]].end_from_Di();
+		//t.endCriteria.angle.set(atan2(end_from_Di.p.y, end_from_Di.p.x));
 
 	}
 	debug::print_pose(t.disturbance.pose(), "new task disturbance is at: ");
