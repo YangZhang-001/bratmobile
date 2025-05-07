0feb6b7a6d2ac8ec9ff1bf3ca9bef75c138f7c6d
glafratta <g.lafratta@outlook.com>
2025-05-07 13:47:28 +0100

    increasing I coefficient

--- src/control_interface.h
+++ src/control_interface.h
@@ -40,7 +40,7 @@ public:
 * Output from Configurator to Motors
 */
 class Motor_Out:public IOInterface { 
-	float L=0, R=0, L_gain=1.0f, R_gain=1.0f, alpha_p=0.01, alpha_i=0.02, alpha_d=0.000;
+	float L=0, R=0, L_gain=1.0f, R_gain=1.0f, alpha_p=0.01, alpha_i=0.025, alpha_d=0.000;
 	float prev_error=0;
 	float integral=0;
     public:
