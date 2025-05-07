e3ede7110db1247b943a4df5b5557cf5ad2241e9
glafratta <g.lafratta@outlook.com>
2025-05-07 12:50:45 +0100

    swapped control input signs

--- src/control_interface.h
+++ src/control_interface.h
@@ -68,12 +68,12 @@ class Motor_Out:public IOInterface {
 	void adjust_gain(b2Rot e, b2Rot rot){ //delta rule ()
 		integral+=e.GetAngle();
 		float increment=alpha_p*e.GetAngle() + alpha_i*integral +alpha_d*prev_error-e.GetAngle();
-		// if (e.GetAngle()<0.01){
-			L_gain-=increment;
+		//  if (e.GetAngle()>0){
+			L_gain+=increment/2;
 //		}
-//		else if (e.GetAngle()>0.01){
-			R_gain+=increment;
-//		}
+	//	else if (e.GetAngle()<0){
+			R_gain-=increment/2;
+	//	}
 		prev_error=e.GetAngle();
 	}
 
