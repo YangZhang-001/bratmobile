54bba711181844abb6eda435d96ff0862d1ee8c7
glafratta <g.lafratta@outlook.com>
2025-05-07 18:23:41 +0100

    swaped pose and deltaPose

--- src/CloCK_math.cpp
+++ src/CloCK_math.cpp
@@ -2,7 +2,7 @@
 
 
 void math::applyAffineTrans(const b2Transform& deltaPose, b2Transform& pose){
-	pose =b2Mul(deltaPose, pose);
+	pose =b2Mul(pose, deltaPose);
 }
 
 void math::applyAffineTrans(const b2Transform& deltaPose, State& state){
