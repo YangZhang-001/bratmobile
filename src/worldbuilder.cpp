91f442e384291684d1808d640915b639e727dba3
glafratta <g.lafratta@outlook.com~>
2025-05-08 17:04:16 +0100

    use mulT to find turn

--- src/worldbuilder.cpp
+++ src/worldbuilder.cpp
@@ -7,7 +7,7 @@ void calc_transform(b2Transform & result, b2Transform t_new, b2Transform t_prev)
     float angle=0;
     if (fabs(cos_angle)<=1){
         angle=acosf(cos_angle); //[0, pi]
-        b2Transform pov_prev=b2MulT(t_prev.p, t_new.p); //position of new d from prev perspective
+        b2Transform pov_prev=b2MulT(t_prev, t_new); //position of new d from prev perspective
         if (pov_prev.p.y<0){
             angle-=angle;
         }
