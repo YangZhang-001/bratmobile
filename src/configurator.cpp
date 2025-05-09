3a450375da0dc1bd5df2f4197bcd8cc35b2a5679
glafratta <g.lafratta@outlook.com>
2025-05-09 16:29:05 +0100

    making task sensor after resetting goal, taking not current dist pose but from_Di

--- src/configurator.cpp
+++ src/configurator.cpp
@@ -1041,10 +1041,11 @@ void Configurator::change_task(){
 	//this is to adjust goal expectation (i.e. the goal sits a certain transfomr away from Di)
 	if (controlGoal.getAffIndex()==PURSUE && !plan.empty()){
 		b2Transform Di_to_end=b2MulT(currentTask.disturbance.pose(), transitionSystem[plan[plan.size()-1]].Di.pose()); //assumes that the last step in the plan reaches the goal
-		b2Transform sum_transform=currentTask.disturbance.from_Di()+Di_to_end; //where goal should be
+		b2Transform sum_transform=currentTask.from_Di()+Di_to_end; //where goal should be
 		b2Transform difference=controlGoal.disturbance.pose()-sum_transform; //difference in pose
 		math::applyAffineTrans(difference, &controlGoal);//update goal with ratio info
 	}
+	debug::print_pose(controlGoal.disturbance.pose, "new gaol pose:");
 	task_sensor=worldBuilder.sensor_box(Robot::get_vertices(),b2Transform_zero, &(controlGoal.disturbance));
 	return;
 }
