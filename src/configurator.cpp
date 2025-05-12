94cf33d7834b698c098993c9bdc414e43520b291
glafratta <g.lafratta@outlook.com>
2025-05-12 18:40:21 +0100

    resetting error

--- src/configurator.cpp
+++ src/configurator.cpp
@@ -1033,7 +1033,7 @@ void Configurator::change_task(){
 	printf("change!\n");
 	next_task();
 	worldBuilder.wb_bridger.set_tracked_disturbance(currentTask.disturbance);
-	control->reset();
+	control->reset_error();
 	control->getData(currentTask.action);
 	return;
 }
