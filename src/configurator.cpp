c1bcbcf68ca7fbe4ee3f7d7630786891151f07ed
glafratta <g.lafratta@outlook.com>
2025-05-14 19:10:25 +0100

    scrap the transform

--- src/configurator.cpp
+++ src/configurator.cpp
@@ -1061,10 +1061,10 @@ void Configurator::adjust_goal_expectation(){
 			from_Di=currentTask.from_Di();
 		//}
 		b2Transform sum_transform=from_Di+task_controller->to_goal(); //where goal should be
-		//controlGoal.disturbance.bf.pose.p=sum_transform.p;
-		b2Transform difference=controlGoal.disturbance.pose()-sum_transform; //difference in pose
-//		debug::print_pose(difference, "difference between pose and likely goal pose:");
-		math::applyAffineTrans(difference, &controlGoal);//update goal with ratio info
+		controlGoal.disturbance.bf.pose.p=sum_transform.p;
+// 		b2Transform difference=controlGoal.disturbance.pose()-sum_transform; //difference in pose
+// //		debug::print_pose(difference, "difference between pose and likely goal pose:");
+// 		math::applyAffineTrans(difference, &controlGoal);//update goal with ratio info
 	}
 	debug::print_pose(controlGoal.disturbance.pose(), "new gaol pose:");
 
