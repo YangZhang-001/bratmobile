cde42a9394bfeb484b4bf2f425381a0bb5bdd20d
glafratta <g.lafratta@outlook.com>
2025-05-14 17:41:10 +0100

    changed Ki Kp Kd, need to account for execution error! it never endscat /tmp/thresholds.txt cat /tmp/thresholds.txt

--- src/control_interface.h
+++ src/control_interface.h
@@ -40,7 +40,7 @@ public:
 * Output from Configurator to Motors
 */
 class Motor_Out:public IOInterface { 
-	float L=0, R=0, L_gain=1.0f, R_gain=1.0f, alpha_p=0.02, alpha_i=0.03, alpha_d=0.01A;
+	float L=0, R=0, L_gain=1.0f, R_gain=1.0f, alpha_p=0.02, alpha_i=0.03, alpha_d=0.01;
 	float prev_error=0;
 	float integral=0;
     public:
