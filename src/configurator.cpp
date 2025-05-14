f17acf667e89bcb5d131cc92aaa961d62dddec72
glafratta <g.lafratta@outlook.com>
2025-05-14 16:11:56 +0100

    saving ratio in controller

--- src/configurator.cpp
+++ src/configurator.cpp
@@ -1054,10 +1054,8 @@ void Configurator::update_graph(TransitionSystem&g, const b2Transform & _deltaPo
 
 void Configurator::adjust_goal_expectation(){
 	if (controlGoal.getAffIndex()==PURSUE && !plan.empty()&&task_controller->get_disturbance().getAffIndex()!=NONE){
-		vertexDescriptor plan_end=plan[plan.size()-1];
-		b2Transform Di_to_end=b2MulT(task_controller->get_disturbance().pose() , transitionSystem[plan_end].Di.pose()); //assumes that the last step in the plan reaches the goal
-		debug::print_pose(Di_to_end, "current Di transform from goal:");
-		printf("distance=%f\n", Di_to_end.p.Length());
+		debug::print_pose(task_controller->to_goal(), "current Di transform from goal:");
+		printf("distance=%f\n", task_controller->to_goal().p.Length());
 		b2Transform from_Di=currentTask.from_Di();
 		b2Transform sum_transform=from_Di+Di_to_end; //where goal should be
 		controlGoal.disturbance.bf.pose.p=sum_transform.p;
