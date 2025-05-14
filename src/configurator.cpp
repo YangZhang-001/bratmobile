4b6c24bf6b44a29b85535a8549164a869c535270
glafratta <g.lafratta@outlook.com>
2025-05-14 16:13:23 +0100

    saving ratio in controller

--- src/configurator.cpp
+++ src/configurator.cpp
@@ -1057,7 +1057,7 @@ void Configurator::adjust_goal_expectation(){
 		debug::print_pose(task_controller->to_goal(), "current Di transform from goal:");
 		printf("distance=%f\n", task_controller->to_goal().p.Length());
 		b2Transform from_Di=currentTask.from_Di();
-		b2Transform sum_transform=from_Di+Di_to_end; //where goal should be
+		b2Transform sum_transform=from_Di+task_controller->to_goal(); //where goal should be
 		controlGoal.disturbance.bf.pose.p=sum_transform.p;
 		// b2Transform difference=controlGoal.disturbance.pose()-sum_transform; //difference in pose
 		//debug::print_pose(difference, "difference between pose and likely goal pose:");
