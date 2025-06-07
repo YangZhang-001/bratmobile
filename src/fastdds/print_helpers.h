a7849f6e08433ab1ccedecbf4d19cd55b5338780
glafratta <g.lafratta@outlook.com~>
2025-06-07 15:53:54 +0100

    add print helpers

---
+++ src/fastdds/print_helpers.h
@@ -0,0 +1,19 @@
+#ifndef PRINT_HELPERS_H
+#define PRINT_HELPERS_H
+
+#include <cstdlib>
+
+void print_xy(auto & x, auto & y){
+    std::cout << "x: " << x << "y: " <<y;
+}
+
+void print_bounds(auto &x_low, auto & y_low, auto& x_high, auto& y_high){
+    std::cout << "lower bound: ";
+    print_xy(x_low, y_low);
+    std::cout <<"  upper bound: ";
+    print_xy(x_high, y_high);
+    std::cout<<std::endl;
+}
+
+
+#endif
