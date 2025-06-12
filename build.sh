0894af14952f453e76c9f8a5f6dd9600c0d3ab92
glafratta <g.lafratta@outlook.com~>
2025-06-12 01:36:07 +0100

    not using temp info

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 export TIME="time result\ncmd:%C\nreal %es\nuser %Us \nsys  %Ss \nmemory:%MKB \ncpu %P"
 temp=$(head -n 1 /sys/class/thermal/thermal_zone0/temp)
 
-if [ $temp -lt 80000 ]
-then
-	echo "temperature of $temp ok, building"
+# if [ $temp -lt 80000 ]
+# then
+# 	echo "temperature of $temp ok, building"
 	sudo rm target targetless test/closed_loop_task CMakeCache.txt Makefile
 	sudo rm /tmp/graph*
 	cmake .
@@ -13,6 +13,6 @@ then
 	cd ..
 	sudo make
 
-else
-	echo "too hot! temp = $temp , not building"
-fi
+# else
+# 	echo "too hot! temp = $temp , not building"
+# fi
