2dce277468020e4351d14a74de01114aeeaa3fa4
glafratta <g.lafratta@outlook.com~>
2025-06-13 19:13:40 +0100

    I am truly lost, added address sanitiser

--- test/unit_test/GTest_Configurator.cpp
+++ test/unit_test/GTest_Configurator.cpp
@@ -1,8 +1,19 @@
 #include "test_classes.h"
 #include <gtest/gtest.h>
 
-TEST(Initialisation, Constructor){
-    DebugConfigurator configurator();
+TEST(Initialisation, ActualConstructor){
+    Configurator configurator;
+}
+
+TEST(Initialisation, DebugConstructor){
+    DebugConfigurator configurator;
+}
+
+TEST_F(ConfiguratorTest, Initialisation){
+}
+
+TEST(Initialisation, Task){
+    Task task;
 }
 
 TEST(Initialisation, InitialMap){
