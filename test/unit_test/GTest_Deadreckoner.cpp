c3437435a8978b410a9f5229a9ac908570fc1542
glafratta <g.lafratta@outlook.com~>
2025-06-13 22:07:22 +0100

    do while

--- test/unit_test/GTest_Deadreckoner.cpp
+++ test/unit_test/GTest_Deadreckoner.cpp
@@ -23,7 +23,7 @@ TEST(DeadReckoning, test){
     motor.getData(action);
     MotorTimer timer(&motor);
     timer.startms(100);
-    d{}
+    do{}
     while(motor.get_motorStep()!=0);
     timer.stop();
     EXPECT_EQ(motor.get_motorStep(),0);
