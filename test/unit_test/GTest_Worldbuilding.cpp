31f7e086be274ec967031015b6a5f7f4960c314b
glafratta <g.lafratta@outlook.com~>
2025-06-23 17:05:37 +0100

    usign pose instead of disturbance

--- test/unit_test/GTest_Worldbuilding.cpp
+++ test/unit_test/GTest_Worldbuilding.cpp
@@ -6,7 +6,7 @@ TEST(Math, affineTransform){
     b2Transform transform(b2Vec2(0.5,0), b2Rot(M_PI_2));
     Task task(disturbance, DEFAULT);
     math::applyAffineTrans(transform, *task.get_disturbance_ptr());
-    EXPECT_FALSE(task.get_disturbance()==disturbance);
+    EXPECT_FALSE(task.get_disturbance().pose()==disturbance.pose());
     
 }
 
