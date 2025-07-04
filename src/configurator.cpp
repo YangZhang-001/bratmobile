cf22c0f30956fb15dd8f891759b401f2abc7a45e
glafratta <g.lafratta@outlook.com~>
2025-07-04 14:35:49 +0100

    seeing visited edges if oe is not empty

--- src/configurator.cpp
+++ src/configurator.cpp
@@ -504,8 +504,8 @@ void AttentiveConfigurator::transitionMatrix(vertexDescriptor v, Direction d, ve
 		}
 		else {
 			if (src==TransitionSystem::null_vertex()){
-				if (!currentTask.get_change() ){
-					auto oe=gt::outEdges(transitionSystem, v, currentTask.get_direction());
+				auto oe=gt::outEdges(transitionSystem, v, currentTask.get_direction());
+				if ( !oe.empty()){ //!currentTask.get_change() ||
 					std::pair<bool, edgeDescriptor> ve=gt::visitedEdge(oe, transitionSystem, currentVertex);
 					if (!ve.first){
 						transitionSystem[v].options={currentTask.get_direction()};
@@ -516,8 +516,8 @@ void AttentiveConfigurator::transitionMatrix(vertexDescriptor v, Direction d, ve
 					}
 				}
 				else{
-					transitionSystem[v].options={DEFAULT, LEFT, RIGHT};
-				}
+				transitionSystem[v].options={DEFAULT, LEFT, RIGHT};
+				}	
 				
 			}
 			else if (temp.getAction().getOmega()!=0){ //if the task chosen is a turning task
