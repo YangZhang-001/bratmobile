8f93a745d90a380230f6efa95edb319ef53d8f23
glafratta <g.lafratta@outlook.com~>
2025-07-08 14:20:27 +0100

    deleted 89 it

--- test/unit_test/GTest_HighLevel.cpp
+++ test/unit_test/GTest_HighLevel.cpp
@@ -180,8 +180,8 @@ INSTANTIATE_TEST_CASE_P(GoalAndMaps, HighLevelTest, ::testing::Values(
                                                                    std::tuple<bool, std::string, int>(true, std::string("../target_40cm/"), 4),
                                                                    std::tuple<bool, std::string, int>(true, std::string("../target_40cm/"), 6), //,
                                                                    std::tuple<bool, std::string, int>(true, std::string("../target_40cm/"), 17),
-                                                                   std::tuple<bool, std::string, int>(true, std::string("../target_40cm/"), 38),
-                                                                   std::tuple<bool, std::string, int>(true, std::string("../target_40cm/"), 89)
+                                                                   std::tuple<bool, std::string, int>(true, std::string("../target_40cm/"), 38)//,
+                                                                  // std::tuple<bool, std::string, int>(true, std::string("../target_40cm/"), 89)
                                                                    ));
 
 
