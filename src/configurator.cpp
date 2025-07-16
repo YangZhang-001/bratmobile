2a0aae9bb3dcd149ba610adb7fb0c2155cac6b93
glafratta <g.lafratta@outlook.com~>
2025-07-16 18:24:48 +0100

    not copying ts

--- src/configurator.cpp
+++ src/configurator.cpp
@@ -940,8 +940,7 @@ bool AttentiveConfigurator::recycle_plan(vertexDescriptor v, vertexDescriptor &v
 	transitionSystem[edge.first].enableOverride();	
 	ExecutionInfo info=package_info(TransitionSystem::null_vertex(), been);
 	info.overarchingGoal(controlGoal_adjusted); 
-	TransitionSystem ts_copy=transitionSystem;
-	auto plan_tmp=planner->plan(ts_copy, v, info, &finished); //not v but task start
+	auto plan_tmp=planner->plan(transitionSystem, v, info, &finished); //not v but task start
 	//printf("out of explore planner\n");
 	bool filler=0;
 	if (finished){
