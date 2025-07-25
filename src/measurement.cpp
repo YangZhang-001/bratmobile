e61a2eb5526458ddc0d48cb1c3dfe4ee429472a6
glafratta <g.lafratta@outlook.com~>
2025-07-25 15:56:10 +0100

    using b2rot multiplication

--- src/measurement.cpp
+++ src/measurement.cpp
@@ -95,7 +95,7 @@ void EndCriteria::adjust(const b2Transform& delta){
     if (angle.isValid()){
         b2Rot newAngle=b2MulT(b2Rot(angle.get_signed()), delta.q);
         //angle.set(angle.get_signed()-delta.q.GetAngle());
-        angle.set(newAngle.GetAngle());
+       angle.set(newAngle.GetAngle());
     }
     if (distance.isValid()){
         distance.set(distance.get_signed()-delta.p.Length());
