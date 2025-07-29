54bbc41d248a16e4146ac631c625e45e5480ea28
glafratta <g.lafratta@outlook.com~>
2025-07-29 19:25:22 +0100

    setting moving vertex endPOse

--- test/unit_test/GTest_Configurator.cpp
+++ test/unit_test/GTest_Configurator.cpp
@@ -414,6 +414,7 @@ INSTANTIATE_TEST_CASE_P(AdjustGoal,
 
 TEST_P(ConfiguratorTest32DT, splitTask){
     //b2Transform start=std::get<0>(GetParam());
+    transitionSystem[MOVING_VERTEX].endPose=std::get<0>(GetParam());
     vertexDescriptor v1=make_v1_crashed(MOVING_VERTEX, std::get<0>(GetParam()), std::get<1>(GetParam()), std::get<2>(GetParam())).m_target;
     std::vector <vertexDescriptor> split =splitTask(v1, transitionSystem[v1].direction, currentVertex);
     b2Vec2 endPosition=std::get<1>(GetParam()).p;
