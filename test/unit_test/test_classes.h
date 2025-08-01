40701248a7007ac3da3f53a557948befbd2921e1
glafratta <g.lafratta@outlook.com~>
2025-08-01 14:16:52 +0100

    not using robot vertices just corner from the centroid

--- test/unit_test/test_classes.h
+++ test/unit_test/test_classes.h
@@ -792,8 +792,10 @@ Pointf HighLevelInterruptBase::generateInterruptingPoint(int taskOrder){
 
     }
     //get task order length
-
-    b2Vec2 pt0(-(Robot::get_vertices()[0].Length()-0.01), 0);
+    //bottom left
+    b2Vec2 cornerFromCentroid(ROBOT_HALFWIDTH, ROBOT_HALFLENGTH);   
+//    b2Vec2 pt0(-(Robot::get_vertices()[0].Length()-0.01), 0);
+    b2Vec2 pt0(-(cornerFromCentroid.Length()-0.01), 0);
     b2Vec2 pt;
     if (configurator->vertex_get_direction(vertexToInterrupt)==DEFAULT){
         pt.x=configurator->vertex_get_endPose(vertexToInterrupt).p.x;
