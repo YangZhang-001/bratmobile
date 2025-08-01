53a4031edbb43be7da3f829ef8175c507d228247
glafratta <g.lafratta@outlook.com~>
2025-08-01 17:02:02 +0100

    no dot

--- test/unit_test/GTest_HighLevel.cpp
+++ test/unit_test/GTest_HighLevel.cpp
@@ -283,12 +283,12 @@ TEST_F(HighLevelTest, BoxedIn){
 
 
 INSTANTIATE_TEST_CASE_P(CulDeSacTurning, HighLevelInterruptTest, testing::Combine(::testing::Values(false), 
-                                                                           ::testing::Values(std::string("../cul_de_sac/"), SYNTH_DATA_FOLDER),
+                                                                           ::testing::Values(std::string("../cul_de_sac/")),
                                                                            ::testing::Values(2),
                                                                            ::testing::Values(-1, 0) ));
 
 INSTANTIATE_TEST_CASE_P(CulDeSacAvoided, HighLevelInterruptTest, testing::Combine(::testing::Values(false), 
-                                                                           ::testing::Values(std::string("../cul_de_sac/"), SYNTH_DATA_FOLDER),
+                                                                           ::testing::Values(std::string("../cul_de_sac/")),
                                                                            ::testing::Values(30),
                                                                            ::testing::Values(-1) ));
 
