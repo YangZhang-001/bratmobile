0f37bb23750258e1ffad945d74949f18a2b44b06
glafratta <g.lafratta@outlook.com~>
2025-08-16 19:36:50 +0100

    need to fix inheritance so i dont have to rewrite everything

---
+++ test/unit_test/GTest_B2B.cpp
@@ -0,0 +1,172 @@
+#include "test_classes.h"
+
+class DebugB2B: public B2BConfigurator{
+    protected:
+        /**
+     * @brief Makes a basic expansion module,  disturbances not set. Module looks like this
+     *               
+     *              v2(LEFT)---v3(DEFAULT)
+                   /   
+                 v0 --- q1(DEFAULT)
+                   \
+                    v4(RIGHT) --- v5(DEFAULT)
+     * 
+     * Not visited by default
+     */
+    void make_module(vertexDescriptor mv=0);
+    public:
+    DebugB2B()=default;
+    
+};
+
+class DebugB2BTest: public DebugB2B, public testing::Test{
+    protected: 
+    void SetUp() override {
+    }
+    void TearDown() override {
+        transitionSystem.clear();
+        transitionSystem=TransitionSystem(1);
+    }
+
+
+};
+
+TEST_F(DebugB2BTest, PartiallyExplore0) {
+    make_module(MOVING_VERTEX);
+    transitionMatrix(MOVING_VERTEX, DEFAULT, MOVING_VERTEX);
+    EXPECT_EQ(transitionSystem[MOVING_VERTEX].options.size(), 0);
+}
+
+TEST_F(DebugB2BTest, PartiallyExplore1) {
+    make_module(MOVING_VERTEX);
+    transitionSystem[3].outcome=simResult::crashed;
+    transitionMatrix(MOVING_VERTEX, DEFAULT, MOVING_VERTEX);
+    EXPECT_EQ(transitionSystem[MOVING_VERTEX].options.size(), 1);
+}
+
+TEST_F(DebugB2BTest, PartiallyExplore2) {
+    make_module(MOVING_VERTEX);
+    transitionSystem[3].outcome=simResult::crashed;
+    transitionSystem[5].outcome=simResult::crashed;
+    transitionMatrix(MOVING_VERTEX, DEFAULT, MOVING_VERTEX);
+    EXPECT_EQ(transitionSystem[MOVING_VERTEX].options.size(), 2);
+}
+
+class HighLevelTestB2B: public HighLevelTest{
+    protected:
+    DebugB2B * configurator=NULL;
+    void SetUp()override{
+        configurator=new DebugB2B();
+        init();
+    }
+};
+
+TEST_P(HighLevelTestB2B, FirstPlanB2B){
+    Task goal;
+    bool hasGoal=std::get<0>(GetParam()), success=false;
+    if (hasGoal){
+        goal=Task(Disturbance(PURSUE, b2Vec2(1.0,0), 0),DEFAULT);
+    }
+    configurator->init(goal);
+    std::string folder=std::get<1>(GetParam());
+    get_plan(folder);
+    EXPECT_GT(ci.data2fp.size(),0);
+    EXPECT_GT(configurator->data_size(),0);
+    if (!hasGoal){
+        success=configurator->plan_reaches_horizon();
+    }
+    else{
+        success=configurator->plan_reaches_goal();
+    }
+    EXPECT_GT(configurator->get_plan().size(),1);
+    EXPECT_TRUE(success);
+}
+
+TEST_P(HighLevelTestB2B, CheckPlanB2B){
+    const char* info=::testing::UnitTest::GetInstance()->current_test_info()->value_param();
+    Logger logger=makeLogger(info);
+    configurator->register_logger(&logger);
+    Task goal;
+    if (std::get<0>(GetParam())){
+        goal=Task(Disturbance(PURSUE, b2Vec2(1.0,0), 0),DEFAULT);
+    }
+    configurator->init(goal);
+    std::string folder=std::get<1>(GetParam());
+    std::vector<vertexDescriptor> plan= get_plan(folder);
+    int vertices_og=configurator->n_vertices();
+    int iteration=std::get<2>(GetParam());
+    trackFor(iteration);
+    std::vector<vertexDescriptor> updated_plan=get_plan(folder, iteration-1); //map 2
+    EXPECT_EQ(di.get_iteration(), iteration);
+    int vertices_now=configurator->n_vertices();
+    EXPECT_LE(vertices_now, vertices_og);
+    bool planned_to_goal=configurator->getGoal().checkEnded(configurator->get_ts()[*(configurator->get_plan().end()-1)].endPose).ended;
+    bool success=planned_to_goal || configurator->getGoal().checkEnded(configurator->vertex_get_endPose(configurator->get_current_vertex())).ended;
+    EXPECT_TRUE(success);
+}
+
+TEST_P(HighLevelTest, RecycleB2B){
+    const char* info=::testing::UnitTest::GetInstance()->current_test_info()->value_param();
+    Logger logger=makeLogger(info);
+    configurator->register_logger(&logger);
+    Task goal;
+    b2Transform shift=b2Transform_zero;
+    if (std::get<0>(GetParam())){
+        goal=Task(Disturbance(PURSUE, b2Vec2(1.0, 0), 0),DEFAULT);
+    }
+    configurator->init(goal);
+    std::string folder=std::get<1>(GetParam());
+    std::vector<vertexDescriptor> plan= get_plan(folder), finished_plan;
+    EXPECT_GT(configurator->get_plan().size(), 1);
+    vertexDescriptor second_last_v=configurator->get_plan()[configurator->get_plan().size()-2];
+    vertexDescriptor last_v=configurator->get_plan()[configurator->get_plan().size()-1];
+    shift=configurator->vertex_get_endPose(last_v);
+    int vertices_og=configurator->n_vertices();
+    configurator->addIteration(100);
+    configurator->set_current_v(last_v); //simulate plan finished
+    configurator->getTask().set_change(true);
+    configurator->set_plan({});
+    wc.next_task(configurator->getTask(), configurator->getGoal(), configurator->get_ts(), configurator->get_current_vertices(), finished_plan);
+    configurator->getTask().set_change(true);
+    EXPECT_EQ(configurator->getTask().get_direction(), configurator->vertex_get_direction(last_v));
+    EXPECT_TRUE(configurator->getTask().get_disturbance()==configurator->vertex_get_Di(last_v));
+    EXPECT_EQ(configurator->get_current_vertex(), last_v);
+    math::MulT(shift, configurator->get_ts());
+    b2Transform newStart=configurator->vertex_get_endPose(last_v);
+    EXPECT_LT(newStart.p.Length(),0.0001);
+    EXPECT_LT(newStart.q.GetAngle(),0.0001);
+    std::vector<vertexDescriptor> updated_plan=get_plan(folder); //map 2
+    int vertices_now=configurator->n_vertices();
+    EXPECT_LE(vertices_now, vertices_og);
+    bool planned_to_goal=configurator->getGoal().checkEnded(configurator->get_ts()[*(configurator->get_plan().end()-1)].endPose).ended;
+    EXPECT_TRUE(planned_to_goal);
+}
+
+INSTANTIATE_TEST_CASE_P(CulDeSac, HighLevelTestB2B, ::testing::Combine( ::testing::Values(false), ::testing::Values(std::string("../cul_de_sac/")), ::testing::Values(2, 3, 4, 17, 36)));
+                                                                  
+
+INSTANTIATE_TEST_CASE_P(Target40, HighLevelTestB2B, ::testing::Combine( ::testing::Values(true), ::testing::Values(std::string("../target_40cm/")), ::testing::Values(2, 3, 4, 6,17, 89)));
+
+INSTANTIATE_TEST_CASE_P(Target68, HighLevelTestB2B, ::testing::Combine( ::testing::Values(true), ::testing::Values(std::string("../target_68cm/")), ::testing::Values(2, 3, 4, 6, 17, 36)));
+
+
+void DebugB2B::make_module(vertexDescriptor mv){
+    //mv=currentVertex;
+    std::vector<vertexDescriptor>new_vertices;
+    for (int i=0; i<5; i++){
+        new_vertices.push_back(boost::add_vertex(transitionSystem));
+    }
+    // vertexDescriptor nv=n_vertices()-1;
+    transitionSystem[new_vertices[0]].direction=DEFAULT;
+    transitionSystem[new_vertices[2]].direction=DEFAULT;
+    transitionSystem[new_vertices[4]].direction=DEFAULT;
+    transitionSystem[new_vertices[1]].direction=LEFT;
+    transitionSystem[new_vertices[3]].direction=RIGHT;
+
+    add_edge_withPoses(mv,new_vertices[0]);
+    add_edge_withPoses(mv,new_vertices[1]);
+    add_edge_withPoses(mv,new_vertices[3]);
+    add_edge_withPoses(new_vertices[1],new_vertices[2]);
+    add_edge_withPoses(new_vertices[3],new_vertices[4]);
+}
+
