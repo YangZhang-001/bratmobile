62215d49e0892fa7a83d4bfe5d9163ffab65f64e
glafratta <g.lafratta@outlook.com~>
2025-08-18 15:25:28 +0100

    trying

--- test/unit_test/GTest_B2B.cpp
+++ test/unit_test/GTest_B2B.cpp
@@ -81,9 +81,10 @@ TEST_F(DebugB2BTest, PartiallyExplore2) {
 
 class HighLevelTestB2B:  public virtual HighLevelTestBase , public testing::WithParamInterface<std::tuple<bool, std::string, int>>{
     void SetUp()override{
-       configurator=new DebugB2B();
+       configurator=new DebugConfigurator();
        init();
     }
+
     void TearDown() override {
         HighLevelTestBase::TearDown();
     }
