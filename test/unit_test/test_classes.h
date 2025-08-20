0affc771bfe8dfc0240b1e19f80cca43fe9040a8
glafratta <g.lafratta@outlook.com~>
2025-08-20 16:01:55 +0100

    made separte conf object of the right type

--- test/unit_test/test_classes.h
+++ test/unit_test/test_classes.h
@@ -447,7 +447,8 @@ class HighLevelTest: public virtual HighLevelTestBase , public testing::WithPara
 };
 
 class HighLevelTestB2B:  public virtual HighLevelTestBase , public testing::WithParamInterface<std::tuple<bool, std::string, int>>{
-
+    protected:
+    DebugB2B * configurator=NULL;
     
     public:
     HighLevelTestB2B(){};
