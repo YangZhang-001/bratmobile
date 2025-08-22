779d91ca1895269754211084a514d54a9ff16593
glafratta <g.lafratta@outlook.com~>
2025-08-22 16:49:14 +0100

    commented

--- test/unit_test/GTest_B2B.cpp
+++ test/unit_test/GTest_B2B.cpp
@@ -435,7 +435,7 @@ INSTANTIATE_TEST_CASE_P(Target40, HighLevelTestB2B, ::testing::Combine( ::testin
 
 INSTANTIATE_TEST_CASE_P(Target68, HighLevelTestB2B, ::testing::Combine( ::testing::Values(true), ::testing::Values(std::string("../target_68cm/")), ::testing::Values(2, 3, 4, 6, 17, 36)));
 
-// TEST_F(HighLevelTest, TrickyScenarioB2B){
+// TEST_F(HighLevelTestB2B, TrickyScenarioB2B){
 //     const char* info=::testing::UnitTest::GetInstance()->current_test_info()->value_param();
 //     Logger logger=makeLogger(info);
 //     configurator->register_logger(&logger);
