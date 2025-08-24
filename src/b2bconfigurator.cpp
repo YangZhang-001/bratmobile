a9d571db3459d98ff09088d7aa9254e03f5dd3c4
glafratta <g.lafratta@outlook.com~>
2025-08-24 20:20:20 +0100

    basically same format getDisturbance as last successful commit

--- src/b2bconfigurator.cpp
+++ src/b2bconfigurator.cpp
@@ -119,15 +119,15 @@ Disturbance B2BConfigurator::getDisturbance(TransitionSystem&g,vertexDescriptor 
 		if (visited.first ||out.empty()){
 			 if (g[v].Di.isValid() && g[v].Di.getAffIndex()==AVOID && (g[v].direction!=dir || (g[v].isTurning() && isTurning(dir)))){ //if Di is valid and not the same direction as the vertex || (g[v].isTurning() && isTurning(dir))
 				Disturbance Di= g[v].Di;
-				// Task task(Di, DEFAULT, g[v].endPose, true);
-				// Robot robot(&world);
-				// robot.body()->SetTransform(task.getStart().p, task.getStart().q.GetAngle());
-				// b2AABB box =worldBuilder.makeRobotSensor(robot.body(), controlGoal.get_disturbance_ptr());
-				// b2Fixture *sensor =GetSensor(robot.body());
-				// bool overlap=overlaps(robot.body(), &Di) && sensor;
-				// world_cleanup(world);
-				if (attentionWindowOverlaps(Di, g[v].endPose, world, controlGoal.get_disturbance_ptr())){
-				//if (overlap){
+				Task task(Di, DEFAULT, g[v].endPose, true);
+				Robot robot(&world);
+				robot.body()->SetTransform(task.getStart().p, task.getStart().q.GetAngle());
+				b2AABB box =worldBuilder.makeRobotSensor(robot.body(), controlGoal.get_disturbance_ptr());
+				b2Fixture *sensor =GetSensor(robot.body());
+				bool overlap=overlaps(robot.body(), &Di) && sensor;
+				world_cleanup(world);
+				//if (attentionWindowOverlaps(Di, g[v].endPose, world, controlGoal.get_disturbance_ptr())){
+				if (overlap){
 					Di.bf.pose=b2Mul(invmul, Di.bf.pose);
 					return Di;
 				}
@@ -135,7 +135,7 @@ Disturbance B2BConfigurator::getDisturbance(TransitionSystem&g,vertexDescriptor 
 			//check if Di was eliminated
 			return controlGoal.get_disturbance();
 		}
-		else if (Disturbance CVDi=clearvoyance.query(v); CVDi.isValid() && g[v].direction==dir){
+		else if (Disturbance CVDi=clearvoyance.query(v); CVDi.isValid() ){ //&& g[v].isTurning()==isTurning(dir)
 			CVDi.bf.pose= b2Mul(invmul, CVDi.bf.pose);
 			return CVDi;
 		}
