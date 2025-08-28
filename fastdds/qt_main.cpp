18e432bc7aaf2d765ac91b7196865449a7f277dc
glafratta <g.lafratta@outlook.com~>
2025-08-28 10:53:41 +0100

    moved fastdds to bratmobile folder

---
+++ fastdds/qt_main.cpp
@@ -0,0 +1,16 @@
+
+#include <QApplication>
+#include "window.h"
+
+int main(int argc, char *argv[])
+{
+	QApplication app(argc, argv);
+
+	Window window;
+	// create the window as a callback for the subscriber
+	window.show();
+
+	window.start();
+
+	return app.exec();
+}
