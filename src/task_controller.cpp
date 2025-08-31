539443921e9d13ed95eaeaecefa8293ea072d1fc
glafratta <g.lafratta@outlook.com>
2025-08-31 23:15:14 +0100

    actually using control goal pose instead of end of plan?

--- src/task_controller.cpp
+++ src/task_controller.cpp
@@ -71,7 +71,10 @@ Task Wise_Controller::task_to_execute(const std::vector<vertexDescriptor>&p, con
         disturbance_q=g[p[0]].Di; 	
     }
 	vertexDescriptor plan_end=p[p.size()-1];
-	_D_to_goal=b2MulT(disturbance_q.pose() , g[plan_end].Di.pose()); //assumes that the last step in the plan reaches the goal
+	// _D_to_goal=b2MulT(disturbance_q.pose() , g[plan_end].Di.pose()); //assumes that the last step in the plan reaches the goal
+	if (controlGoal.get_disturbance().getAffIndex()!=NONE){
+		_D_to_goal=b2MulT(disturbance_q.pose() , controlGoal.get_disturbance().pose()); //assumes that the last step in the plan reaches the goal
+	}
 	t.setMotorStep(motor_step(t.getAction(), start_to_end.p.Length()));
 	// printf("new disturbance x=%f \t y=%f \t %theta=%f\n", t.get_disturbance().pose().p.x, t.get_disturbance().pose().p.y, t.get_disturbance().pose().q.GetAngle() );
 	// printf("goal x=%f \t y=%f \t %theta=%f\n", g[plan_end].Di.pose().p.x, g[plan_end].Di.pose().p.y, g[plan_end].Di.pose().q.GetAngle() );
