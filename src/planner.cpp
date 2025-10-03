3c2278764f016c7234dbc7c088ac0e4a46c6e9b2
glafratta <g.lafratta@outlook.com>
2025-10-03 13:54:25 +0100

    for frontier vertices, ensuring that frontier doesn't have an invalid it_observed

--- src/planner.cpp
+++ src/planner.cpp
@@ -102,7 +102,8 @@ std::vector <Frontier> frontierVertices(vertexDescriptor v, TransitionSystem& g,
 			std::vector <vertexDescriptor>connecting2;
 			NotSelfEdge not_self_edge(&g);
 			do {
-				if ((g[(*ei3).m_target].visited() || info.been())&& (not_self_edge(*ei3) || g[*ei3].overrideZeroSteps)){ //(*ei3).m_source!=(*ei3).m_target
+				//g[(*ei3).m_target].visited()
+				if ((g[(*ei3)].it_observed>0 || info.been())&& (not_self_edge(*ei3) || g[*ei3].overrideZeroSteps)){ //(*ei3).m_source!=(*ei3).m_target
 					if (!g[(*ei3).m_target].visited()){
 						EndedResult er = estimateCost(g[(*ei3).m_target], g[(*ei3).m_source].endPose, g[(*ei3).m_target].direction,info.overarchingGoal());
 						std::vector<vertexDescriptor>_plan=info.plan();
