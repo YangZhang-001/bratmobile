516e0444abd287ce3a29bc3d5411f0e301bc1e5e
glafratta <g.lafratta@outlook.com>
2025-10-04 01:32:34 +0100

    aliminated checking if plan[0]==0 and returned to mult mult instead of inv

--- src/attentive.cpp
+++ src/attentive.cpp
@@ -584,7 +584,7 @@ bool AttentiveConfigurator::recycle_plan(vertexDescriptor v, vertexDescriptor &v
 	Task controlGoal_adjusted= controlGoal;
 	//position of task start with respect to goal disturbance (pov)
 //	shift_start= b2MulT(b2MulT(sk_first_start, controlGoal.getStart()), transitionSystem[task_start].start);
-	shift_start= b2Mul(InvMul(sk_first_start, controlGoal.getStart()), transitionSystem[task_start].start);
+	shift_start= b2MulT(b2MulT(sk_first_start, controlGoal.getStart()), transitionSystem[task_start].start);
 	Mul(shift_start, controlGoal_adjusted);
 	if (edge.first.m_source!=v0){
 		boost::remove_edge(edge.first, transitionSystem);
