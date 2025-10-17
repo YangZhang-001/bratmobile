330c460141c2b389e670c7077d912a1fe19bbd67
glafratta <g.lafratta@outlook.com>
2025-10-17 10:42:55 +0100

    added brat4

---
+++ brat4_target.cpp
@@ -0,0 +1,37 @@
+#include "custom_robot.h"
+
+int main(int argc, char** argv) {
+	std::cout<<"Navigating to Target with Brat2"<<std::endl;
+	A1Lidar lidar;
+	AlphaBot motors;
+	Disturbance target(2, b2Vec2(BOX2DRANGE, 0));
+    Task controlGoal(target, DEFAULT);
+	LIDAR_In configuratorInterface;
+	//Motor_Out controlInterface;
+    FocusedConfigurator configurator(controlGoal);
+	configuratorInterface.register_worldBuilder(new LaserFocus());
+	HorizonStarPlanner planner;
+	OpenLooper tracker;
+	configurator.register_planner(&planner);
+	configurator.register_tracker(&tracker);
+	OpenLoopController wc;
+	configurator.register_controller(&wc);
+	Logger logger( "brat2-target", "/tmp");
+	configurator.register_logger(&logger);
+	configurator.setSimulationStep(.27);
+	LidarInterface dataInterface(&configuratorInterface);
+	configurator.registerInterface(&configuratorInterface, &tracker);
+	MotorCallback cb(&controlInterface);
+	lidar.registerInterface(&dataInterface);
+	motors.registerStepCallback(&tracker);
+	configurator.start();
+	lidar.start();
+	motors.start();
+	getchar();
+	motors.stop();
+	configurator.stop();
+	lidar.stop();
+	logger.~Logger();
+}
+	
+	
---
+++ brat4_targetless.cpp
@@ -0,0 +1,55 @@
+#include "custom_robot.h"
+
+class NoGoal:public GoalChanger{
+
+	Task change_goal(const Task & task){
+		return Task();
+	}
+
+};
+
+class CLTracker: public ClosedLoop_Tracker, public MotorCallback, public Motor_Out{
+	public:
+	CLTracker():MotorCallback(this){}
+};
+
+int main(int argc, char** argv) {
+	A1Lidar lidar;
+	AlphaBot motors;
+	Disturbance target(2, b2Vec2(BOX2DRANGE, 0));
+    Task controlGoal(target, DEFAULT);
+	LIDAR_In configuratorInterface;
+	//Motor_Out controlInterface;
+    FocusedConfigurator configurator(controlGoal);
+	NoGoal goalChanger;
+	configurator.register_goalChanger(&goalChanger);
+	HorizonStarPlanner planner;
+	OpenLooper tracker;
+	configurator.register_planner(&planner);
+	configurator.register_tracker(&tracker);
+	OpenLoopController wc;
+	configurator.register_controller(&wc);
+	Logger logger( "rt-update-targetless", "/tmp");
+	configurator.register_logger(&logger);
+	configurator.setSimulationStep(.27);
+	LidarInterface dataInterface(&configuratorInterface);
+	configurator.registerInterface(&configuratorInterface, &tracker);
+	MotorCallback cb(&controlInterface);
+	lidar.registerInterface(&dataInterface);
+	motors.registerStepCallback(&tracker);
+	printf("all registered\n");
+	configurator.start();
+	lidar.start();
+	motors.start();
+	getchar();
+	motors.stop();
+	configurator.stop();
+	lidar.stop();
+	logger.~Logger();
+}
+	
+	
+
+	
+
+	
