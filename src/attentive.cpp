03a47f57a7c9b1c1dd98c1f1a93949bd28aec7af
glafratta <g.lafratta@outlook.com>
2025-10-17 22:11:30 +0100

    ok no longer enforcing but just using distance to simstep

--- src/attentive.cpp
+++ src/attentive.cpp
@@ -855,11 +855,8 @@ void AttentiveConfigurator::EvaluationQueueManager::addToEvaluationQueue(std::ve
 void AttentiveConfigurator::enforce_edge(){
 	if (m_plan.empty()){return;}
 	auto edge=gt::add_edge(MOVING_VERTEX, m_plan[0], transitionSystem, iteration);
-	if (edge.second){
-		std::cout<<"enforcing moving edge from "<<MOVING_VERTEX<<" to "<<m_plan[0]<<std::endl;
-		transitionSystem[edge.first].step=gt::distanceToSimStep(transitionSystem[m
 }
-}
+
 
 void FocusedConfigurator::removeExploredTransitions(vertexDescriptor v){
 	AttentiveConfigurator::removeExploredTransitions(v);
