8cc555490243bdc3aab842eb532c67ccc8dc1bdd
glafratta <g.lafratta@outlook.com>
2025-10-22 18:36:22 +0100

    using task change not executingThisTask

--- src/attentive.cpp
+++ src/attentive.cpp
@@ -897,8 +897,8 @@ void DiscreteConfigurator::transitionMatrix(vertexDescriptor v, Direction d, ver
 	Task temp(controlGoal.get_disturbance(), DEFAULT, transitionSystem[v].endPose); //reflex to disturbance
 	srand(unsigned(time(NULL)));
 	auto oe=gt::outEdges(transitionSystem, v, d);
-	bool executingThisTask=( !currentTask.get_change()&& oe.empty()) && (iteration>1);
-	if (!isTurning(d) &&!executingThisTask && temp.getAction().getOmega()==0 && transitionSystem[v].outcome==simResult::successful){
+	//bool executingThisTask= currentTask.get_change();
+	if (!isTurning(d) &&currentTask.get_change() && temp.getAction().getOmega()==0 && transitionSystem[v].outcome==simResult::successful){
 		int random= rand();
 		if (random%2==0){
 			transitionSystem[v].options.push_back(LEFT);// = {DEFAULT, LEFT, RIGHT};
