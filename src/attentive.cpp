09f3ed4af97647d5b92970c06620520bd5d7dae0
glafratta <g.lafratta@outlook.com>
2025-10-25 14:49:22 +0100

    just not reassigning result..

--- src/attentive.cpp
+++ src/attentive.cpp
@@ -463,18 +463,16 @@ VertexMatch AttentiveConfigurator::findMatch(State s, Direction dir, StateMatche
 		}
 		condition=matcher.match_equal(m, match_type);
 		if (v!=MOVING_VERTEX && (boost::in_degree(v, transitionSystem)>0 || iteration>1)  &&Tmatch ){ 
-			// if (condition){
-			// 	result.first= m;
-			// 	result.second=v;
-			// 	VertexMatchTuple to_add(v, m, sum_tmp);
-			// 	others_set.emplace(to_add);
-			// }
+			if (condition){
+				// result.first= m;
+				// result.second=v;
+				VertexMatchTuple to_add(v, m, sum_tmp);
+				others_set.emplace(to_add);
+			}
 			if (sum_tmp<sum){
 				sum=sum_tmp;
 				result.first=m;
 				result.second=v;
-				VertexMatchTuple to_add(v, m, sum_tmp);
-				others_set.emplace(to_add);
 				if (NULL!=_sd){
 					*_sd=sd;
 				}
