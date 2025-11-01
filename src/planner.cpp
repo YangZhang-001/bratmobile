d0bfa57077cd612f1e3277acaaaed70da6a73d63
glafratta <g.lafratta@outlook.com>
2025-11-01 23:35:11 +0000

    not sure why it says std::find returns bool

--- src/planner.cpp
+++ src/planner.cpp
@@ -162,8 +162,8 @@ std::vector <Frontier> frontierVertices(vertexDescriptor v, TransitionSystem& g,
 
 void HorizonStarPlanner::addToPriorityQueue(const Frontier& f, std::vector<Frontier>& queue, TransitionSystem &g, std::vector<vertexDescriptor>& closed, vertexDescriptor goal){
 	for (auto i =queue.begin(); i!=queue.end(); i++){
-		std::vector<vertexDescriptor>::iterator it=std::find(closed.begin(), closed.end(), f.frontier)!=closed.end();
-		if (g[f.frontier].phi <abs(g[(*i).frontier].phi)&& it!=closed.end()){
+		auto it=std::find(closed.begin(), closed.end(), f.frontier);
+		if (g[f.frontier].phi <abs(g[(*i).frontier].phi)&& it){
 			queue.insert(i, f);
 			return;
 		}
