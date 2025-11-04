4cf8a1415765fd429651e6516a8e7f70366ac189
glafratta <g.lafratta@outlook.com>
2025-11-04 20:15:01 +0000

    added closed

--- src/planner.cpp
+++ src/planner.cpp
@@ -166,7 +166,7 @@ std::vector <Frontier> frontierVertices(vertexDescriptor v, TransitionSystem& g,
 
 void HorizonStarPlanner::addToPriorityQueue(const Frontier& f, std::vector<Frontier>& queue, TransitionSystem &g,const  std::vector<vertexDescriptor>&closed, vertexDescriptor goal){
 	for (auto i =queue.begin(); i!=queue.end(); i++){
-		auto it=std::find(closed.begin(), closed.end(), *i);
+		auto it=std::find(closed.begin(), closed.end(), i->frontier);
 		if (g[f.frontier].phi <abs(g[(*i).frontier].phi) && it==closed.end()){
 			queue.insert(i, f);
 			return;
