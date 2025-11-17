f624b63d05edf4953b7f69985541af8cee92b4f3
glafratta <g.lafratta@outlook.com>
2025-11-17 18:00:20 +0000

    not adding to pq if already there!

--- src/planner.cpp
+++ src/planner.cpp
@@ -176,7 +176,8 @@ std::vector <Frontier> frontierVertices(vertexDescriptor v, TransitionSystem& g,
 void HorizonStarPlanner::addToPriorityQueue(const Frontier& f, std::vector<Frontier>& queue, TransitionSystem &g,const  std::vector<vertexDescriptor>&closed, vertexDescriptor goal){
 	for (auto i =queue.begin(); i!=queue.end(); i++){
 		auto it=std::find(closed.begin(), closed.end(), i->frontier);
-		if (it!=closed.end()){
+		auto it_q=std::find(queue.begin(), queue.end(), i->frontier);
+		if (it!=closed.end() || it_q!=queue.end()){
 			return;
 		}
 		if (g[f.frontier].phi <abs(g[(*i).frontier].phi)){
