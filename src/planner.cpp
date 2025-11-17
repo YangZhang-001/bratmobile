3f180a12b327f55c88b8326d4761ff1a58c5129a
glafratta <g.lafratta@outlook.com>
2025-11-17 18:04:22 +0000

    const iterators

--- src/planner.cpp
+++ src/planner.cpp
@@ -176,7 +176,7 @@ std::vector <Frontier> frontierVertices(vertexDescriptor v, TransitionSystem& g,
 void HorizonStarPlanner::addToPriorityQueue(const Frontier& f, std::vector<Frontier>& queue, TransitionSystem &g,const  std::vector<vertexDescriptor>&closed, vertexDescriptor goal){
 	for (auto i =queue.begin(); i!=queue.end(); i++){
 		auto it=std::find(closed.begin(), closed.end(), i->frontier);
-		auto it_q=std::find(queue.begin(), queue.end(), f);
+		auto it_q=std::find(queue.cbegin(), queue.cend(), f);
 		if (it!=closed.end() || it_q!=queue.end()){
 			return;
 		}
