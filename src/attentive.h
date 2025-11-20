c086097e3145857d1f2c910f1ebc9e5ba5b360d3
glafratta <g.lafratta@outlook.com>
2025-11-20 11:32:40 +0000

    focused config not virtual inheritance from attentive

--- src/attentive.h
+++ src/attentive.h
@@ -430,7 +430,7 @@ virtual ~AttentiveConfigurator()=default;
 /** 
  * @brief Configurator that only replans if the current task fails
 */
-class FocusedConfigurator:virtual public AttentiveConfigurator{
+class FocusedConfigurator: public AttentiveConfigurator{
 	protected:
 
 	StateMatcher::MATCH_TYPE desiredMatch() override {
