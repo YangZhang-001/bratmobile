6addfc7d447edae03fb9490b393f261af5a39666
glafratta <g.lafratta@outlook.com>
2025-11-21 19:38:55 +0000

    reactive controler

--- reactive.cpp
+++ reactive.cpp
@@ -22,7 +22,7 @@ int main(int argc, char** argv) {
 	OpenLooper tracker;
 	configurator.register_planner(&planner);
 	configurator.register_tracker(&tracker);
-	Reactive_Controller Controller wc;
+	Reactive_Controller wc;
 	configurator.register_controller(&wc);
 	LidarInterface dataInterface(&configuratorInterface);
 	configurator.registerInterface(&configuratorInterface, &tracker);
