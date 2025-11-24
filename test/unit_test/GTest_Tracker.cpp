462677afd75d5d8552744deb8c2e96fc20a6d9d9
glafratta <g.lafratta@outlook.com>
2025-11-24 15:52:03 +0000

    deleted duplicate test

--- test/unit_test/GTest_Tracker.cpp
+++ test/unit_test/GTest_Tracker.cpp
@@ -299,31 +299,6 @@ TEST_P(TestDeadReckoning, InvMulGoal){
     }
 }
 
-/**
- * @brief Test whether the current invmul method is accurate when turning left
- */
-TEST_P(TestDeadReckoning, InvMulGoal){
-    Task t=DebugConfigurator::generateGoalTask();
-    Task::Action a1, a2;
-    auto directions=GetParam();
-    a1.init(std::get<0>(directions));
-    a2.init(std::get<1>(directions));
-    b2Transform t1=(a1.getTransform(0.1)), t2=a2.getTransform(0.1);
-    for (int i=0; i<20; i++){
-        Configurator::InvMul(t1, t);
-    }
-    for (int i=0; i<20; i++){
-        Configurator::InvMul(t2, t);
-    }
-    float disturbanceAngle=t.get_disturbance().pose().q.GetAngle();
-    float t1Angle=t1.q.GetAngle();
-    if (std::get<0>(directions)==std::get<1>(directions)){
-        EXPECT_EQ(std::signbit(disturbanceAngle), std::signbit(-t1Angle));
-    }
-    else{
-        EXPECT_NEAR(disturbanceAngle, 0, M_PI/10);
-    }
-}
 
 
 
