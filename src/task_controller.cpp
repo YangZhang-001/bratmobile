fe29e3cf24a05adf9353ec887de6c7a67c3dfa2f
glafratta <g.lafratta@outlook.com>
2025-11-27 17:08:15 +0000

    if task motorstep is current task motorstep is zero

--- src/task_controller.cpp
+++ src/task_controller.cpp
@@ -112,7 +112,7 @@ Task Reactive_Controller::next_task( Task currentTask, const Task & controlGoal,
 }
 
 Task OpenLoopController::next_task(Task currentTask, const Task & controlGoal, const TransitionSystem & g, std::vector <vertexDescriptor> & current_vertices, std::vector<vertexDescriptor> & plan){
-	if (plan.empty() && currentTask.get_change()){
+	if (plan.empty() && currentTask.getMotorStep()==0){
 		current_vertices={MOVING_VERTEX};
 		return stopTask(controlGoal);
 	}
