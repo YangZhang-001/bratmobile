5f9a383eec942813577bfe7f1240e39ed91ec86c
glafratta <g.lafratta@outlook.com>
2026-01-21 23:54:14 +0000

    if same task, propagate

--- src/focused.cpp
+++ src/focused.cpp
@@ -271,7 +271,9 @@ bool FocusedConfigurator::propagateD(vertexDescriptor v1, vertexDescriptor v0, s
 		return false;
 	}
 	bool same_Di=transitionSystem[v0].Di==transitionSystem[v1].Di;
-	if (canPropagate(v0)&& same_Di && transitionSystem[v0].Dn.getAffIndex()==NONE){
+	std::vector<vertexDescriptor> tv=task_vertices(v1);
+	bool sameTask=std::find(tv.begin(), tv.end(), v0)!=tv.end();
+	if ((canPropagate(v0)&& same_Di && transitionSystem[v0].Dn.getAffIndex()==NONE) || sameTask){
  			transitionSystem[v0].Dn = transitionSystem[v1].Dn; //was target
  	}
 	bool canReassign=canReassignOutcome(v0);
