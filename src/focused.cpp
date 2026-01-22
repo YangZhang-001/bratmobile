d38acffc7b0e4e507deb9711fac1d75498007870
glafratta <g.lafratta@outlook.com>
2026-01-22 14:30:01 +0000

    only reassigning outcome if new vertex was added

--- src/focused.cpp
+++ src/focused.cpp
@@ -207,7 +207,9 @@ std::vector <vertexDescriptor> FocusedConfigurator::splitTask( vertexDescriptor 
 			if (match.first!=StateMatcher::_TRUE || match.second==v){
 				transitionSystem[v1].endPose = endPose;
 				transitionSystem[first_edge.first].step= gt::distanceToSimStep(transitionSystem[v1].distance(), a.getLinearSpeed());	
-				transitionSystem[v1].outcome=simResult::crashed;	
+				if (first_edge.second){
+					transitionSystem[v1].outcome=simResult::crashed;	
+				}
 				transitionSystem[v1].phi=og_phi;
 			}
 		}
