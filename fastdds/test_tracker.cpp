29a3e742fdd08a84c45d6eef6643da9f71e0e72d
glafratta <g.lafratta@outlook.com>
2026-01-30 12:12:02 +0000

    toggling attention window

--- fastdds/test_tracker.cpp
+++ fastdds/test_tracker.cpp
@@ -6,11 +6,19 @@ class FakeTracking:public CppTimer{
     CLTrackerGUI tracker;
     Disturbance Dg=Disturbance(PURSUE, b2Vec2(1.0f,0.0f));
     Disturbance Di=Disturbance(AVOID, b2Vec2(.4f, .0f));
+    int timerCount=0;
     void timerEvent(){
         tracker.on_new_reading(Task(Dg, DEFAULT), Task(Di, DEFAULT));
         math::MulT(b2Transform(b2Vec2(0, 0), b2Rot(0.15)), Dg);
         math::MulT(b2Transform(b2Vec2(0.0, 0), b2Rot(0.15)), Di);
-        
+        timerCount++;
+        if (timerCount==10){ 
+            tracker.init(Task());
+        }
+        else if (timerCount==20){
+            tracker.init(Task(Dg, DEFAULT));
+            timerCount=0;
+        }
 
     }
 
