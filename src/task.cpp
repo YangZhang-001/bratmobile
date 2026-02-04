5dcaf7d3313d004b7e27c1e5bac81801e7663eea
glafratta <g.lafratta@outlook.com>
2026-02-04 23:26:43 +0000

    if goal is behind doesnt make sense to try to get in line with it

--- src/task.cpp
+++ src/task.cpp
@@ -267,7 +267,7 @@ EndedResult Task::checkEnded(b2Transform robotTransform, Direction dir,bool rela
 			if (robot!=NULL){
 				std::vector <b2Vec2> local_vertices=GetLocalPoints(disturbance.vertices(), robot);
 				b2Vec2 pos_local=*(std::min_element(local_vertices.begin(), local_vertices.end(), CompareX()));
-				r.ended=fabs(round(pos_local.x*100)/100)<=((endCriteria.distance.get()-0.001)/2) && pos_local.y>0; //-0.001 //was /2
+				r.ended=fabs(round(pos_local.x*100)/100)<=((endCriteria.distance.get()-0.001)/2) && disturbance.pose().p.y>0; //-0.001 //was /2
 			}
 			else if (relax){
 				Distance _d(RELAXED_DIST_ERROR_TOLERANCE);
