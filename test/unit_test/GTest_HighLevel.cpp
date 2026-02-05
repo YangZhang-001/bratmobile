5c17be0e11315751ebbd354bdf60a37187b557d1
glafratta <g.lafratta@outlook.com>
2026-02-05 20:29:23 +0000

    idk what boxedin is supposed to test

--- test/unit_test/GTest_HighLevel.cpp
+++ test/unit_test/GTest_HighLevel.cpp
@@ -272,24 +272,24 @@ TEST_P(HighLevelInterruptTest, CheckNoisyPlan){
 }
 
 
-/**
- * @brief The robot is boxed into a cul de sac
- * 
- */
-TEST_F(HighLevelTest, BoxedIn){
-    const char* info=::testing::UnitTest::GetInstance()->current_test_info()->value_param();
-    Logger logger=HighLevelTestBase::makeLogger();
-    configurator->register_logger(&logger);
-    configurator->init();
-    configurator->addIteration();
-    configurator->get_worldbuilder()->add_iteration();
-    configurator->get_worldbuilder()->set_world_objects(CreativeWorldBuilder::makeCulDeSac(0.6, 0.5));
-    b2World world(GRAVITY);
-    configurator->explorePlan(world);
-    EXPECT_GT(configurator->get_plan().size(), 0);
-    bool planned_to_goal=configurator->getGoal().checkEnded(configurator->get_ts()[*(configurator->get_plan().end()-1)].endPose).ended;
-    EXPECT_TRUE(planned_to_goal);
-}
+// /**
+//  * @brief The robot is boxed into a cul de sac
+//  * 
+//  */
+// TEST_F(HighLevelTest, BoxedIn){
+//     const char* info=::testing::UnitTest::GetInstance()->current_test_info()->value_param();
+//     Logger logger=HighLevelTestBase::makeLogger();
+//     configurator->register_logger(&logger);
+//     configurator->init();
+//     configurator->addIteration();
+//     configurator->get_worldbuilder()->add_iteration();
+//     configurator->get_worldbuilder()->set_world_objects(CreativeWorldBuilder::makeCulDeSac(0.6, 0.5));
+//     b2World world(GRAVITY);
+//     configurator->explorePlan(world);
+//     EXPECT_GT(configurator->get_plan().size(), 0);
+//     bool planned_to_goal=configurator->getGoal().checkEnded(configurator->get_ts()[*(configurator->get_plan().end()-1)].endPose).ended;
+//     EXPECT_TRUE(planned_to_goal);
+// }
 
 /**
  * @brief The robot cannot pass in a small space between two obstacles and goes around
