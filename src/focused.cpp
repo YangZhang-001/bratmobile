bd3f4dff9a1ee188d84eec117794f724bb09edc4
glafratta <g.lafratta@outlook.com>
2026-02-05 00:16:51 +0000

    nvm

--- src/focused.cpp
+++ src/focused.cpp
@@ -150,7 +150,7 @@ std::vector<vertexDescriptor> FocusedConfigurator::explorer(vertexDescriptor v, 
 	backtrack(evaluationQueue, priorityQueue, closed, plan_prov, v, startRecycle);
 	bestNext=priorityQueue[0];
 	reassign_direction(bestNext, direction);
-}while( !er.ended); //g[bestNext].options.size()>0 &&
+}while(g[bestNext].options.size()>0 && !er.ended); //
 return plan_prov;
 }
 
