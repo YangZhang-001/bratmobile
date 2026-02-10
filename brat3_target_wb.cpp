7ca9bfcdf9ced102e72e821e1fc9f386a6def758
glafratta <g.lafratta@outlook.com>
2026-02-10 18:44:47 +0000

    new executable with worldbuilder

---
+++ brat3_target_wb.cpp
@@ -0,0 +1,32 @@
+#include "custom_robot.h"
+
+int main(int argc, char** argv) {
+	std::cout<<"Navigating to Target with Brat2"<<std::endl;
+	A1Lidar lidar;
+	AlphaBot motors;
+	Disturbance target(2, b2Vec2(BOX2DRANGE, 0));
+    Task controlGoal(target, DEFAULT);
+    FocusedConfigurator configurator;
+	configurator.init(controlGoal);
+	HorizonStarPlanner planner;
+	OpenLooper tracker;
+	configurator.register_planner(&planner);
+	configurator.register_tracker(&tracker);
+	OpenLoopController wc;
+	configurator.register_controller(&wc);
+	Logger logger( "brat3-target", "/tmp");
+	configurator.register_logger(&logger);
+	configurator.setSimulationStep(.27);
+	LidarInterface dataInterface(&configurator);
+	configurator.registerInterface(&tracker);
+	lidar.registerInterface(&dataInterface);
+	motors.registerStepCallback(&tracker);
+	lidar.start();
+	motors.start();
+	getchar();
+	motors.stop();
+	lidar.stop();
+	logger.~Logger();
+}
+	
+	
