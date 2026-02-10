2405cac7fa216d5b0378023a2a939d76b2858d07
glafratta <g.lafratta@outlook.com>
2026-02-10 13:26:14 +0000

    doing the test stuff

--- test/unit_test/GTest_ForFigures.cpp
+++ test/unit_test/GTest_ForFigures.cpp
@@ -34,6 +34,9 @@ TEST_F(FigureTest, PrintTransform){
     configurator->set_current_v(last_v); //simulate plan finished
     configurator->getTask().set_change(true);
     configurator->set_plan({});
+    goal=Task(Disturbance(PURSUE, b2Vec2(1.2, 0), 0),DEFAULT);
+    configurator->init(goal);
+
     configurator->setTask(wc.next_task(configurator->getTask(), configurator->getGoal(), configurator->get_ts(), configurator->get_current_vertices(), finished_plan));
     configurator->getTask().set_change(true);
     math::MulT(shift, configurator->get_ts());
