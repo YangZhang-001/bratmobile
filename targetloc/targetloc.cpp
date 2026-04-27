9302095d7de91c53406e8dbd052a1f4eac76c60f
Bernd Porr <mail@berndporr.me.uk>
2026-04-27 23:10:04 +0100

    Switched to clangd extension instead of intellisense.

--- targetloc/targetloc.cpp
+++ targetloc/targetloc.cpp
@@ -5,18 +5,23 @@ void TargetLoc::start()
 {
     cm.start();
 
-    cameraL.registerCallback([&](const cv::Mat &left, const libcamera::ControlList &)
-                             { updateImageL(left); });
+    cameraL.registerCallback(
+        [&](const cv::Mat &left, const libcamera::ControlList &) {
+            updateImageL(left);
+        });
 
-    cameraR.registerCallback([&](const cv::Mat &right, const libcamera::ControlList &)
-                             { updateImageR(right); });
+    cameraR.registerCallback(
+        [&](const cv::Mat &right, const libcamera::ControlList &) {
+            updateImageR(right);
+        });
 
-    targetDet.registerDetCallback([&](const std::vector<cv::Point2f> &coords)
-                                  { onTargetDetected(coords); });
+    targetDet.registerDetCallback([&](const std::vector<cv::Point2f> &coords) {
+        onTargetDetected(coords);
+    });
 
     stereo.registerCallback([&](cv::Mat d) {
-	std::lock_guard<std::mutex> guard(disparityData_mutex);
-	currentD = d; 
+        std::lock_guard<std::mutex> guard(disparityData_mutex);
+        currentD = d;
     });
 
     settings.width = 1920;
@@ -38,10 +43,8 @@ void TargetLoc::newScanAvail(C1LidarData (&data)[C1Lidar::nDistance])
 {
     std::lock_guard<std::mutex> guard(lidarData_mutex);
     currentLidarCoords.clear();
-    for (const auto &v : data)
-    {
-        if (v.valid)
-        {
+    for (const auto &v : data) {
+        if (v.valid) {
             currentLidarCoords.push_back({v.x, v.y});
         }
     }
@@ -86,19 +89,20 @@ void TargetLoc::onTargetDetected(const std::vector<cv::Point2f> &contour)
     float avgX = 0;
     contour_mutex.lock();
     scaledContour.clear();
-    for(auto &c:contour) {
-	const int x = c.x * currentD.size().width / settings.width;
-	const int y = c.y * currentD.size().height / settings.height;
-	scaledContour.emplace_back(x,y);
-	printf("[%d,%d]", x, y);
-	avgX = avgX + c.x;
-	i++;
+    for (auto &c : contour) {
+        const int x = c.x * currentD.size().width / settings.width;
+        const int y = c.y * currentD.size().height / settings.height;
+        scaledContour.emplace_back(x, y);
+        printf("[%d,%d]", x, y);
+        avgX = avgX + c.x;
+        i++;
     }
-    contour_mutex.unlock();    
+    contour_mutex.unlock();
     avgX = avgX / i;
     printf("\n");
 
-    if (currentD.empty()) return;
+    if (currentD.empty())
+        return;
 
     // Create mask
     cv::Mat mask = cv::Mat::zeros(currentD.size(), CV_8UC1);
@@ -106,19 +110,19 @@ void TargetLoc::onTargetDetected(const std::vector<cv::Point2f> &contour)
 
     // Draw filled contour for the mask
     std::vector<std::vector<cv::Point>> scaledContours{scaledContour};
-    cv::drawContours(mask, scaledContours,-1, cv::Scalar(255), cv::FILLED);
+    cv::drawContours(mask, scaledContours, -1, cv::Scalar(255), cv::FILLED);
 
     // Compute mean gray value inside contour
     float avgDisp = cv::mean(currentD, mask)[0];
 
     const float targetLocX = disp2meter / avgDisp;
-    const float targetLocY = xpos2meter * ((settings.width/2) - avgX);
+    const float targetLocY = xpos2meter * ((int)(settings.width / 2) - avgX);
 
-    printf("Disparity: %f, Target location: [%f,%f]\n",avgDisp,targetLocX,targetLocY);
+    printf("Disparity: %f, Target location: [%f,%f]\n", avgDisp, targetLocX,
+           targetLocY);
 
     // callback!
-    if (detectionInterface)
-    {
+    if (detectionInterface) {
         detectionInterface->newTargetDetected(targetLocX, targetLocY);
     }
 }
--- targetloc/targetloc.h
+++ targetloc/targetloc.h
@@ -1,71 +1,85 @@
 #pragma once
 
-#include "stereo.h"
-#include "libcam2opencv.h"
-#include "targetdet.h"
-#include <opencv2/opencv.hpp>
-#include <libcamera/libcamera/camera_manager.h>
 #include "c1lidarrpi.h"
+#include "libcam2opencv.h"
+#include "stereo.h"
+#include "targetdet.h"
+#include <libcamera/libcamera/camera_manager.h>
 #include <mutex>
+#include <opencv2/opencv.hpp>
 #include <vector>
-#include <functional>
 
-class TargetLoc : public C1Lidar::DataInterface
-{
-public:
+class TargetLoc : public C1Lidar::DataInterface {
+  public:
     TargetLoc() = default;
 
-    struct DetectionInterface
-    {
+    /**
+     * Callback interface which reports a new target location.
+     */
+    struct DetectionInterface {
+        /**
+         * Callback that a new target has been detected.
+         * @param x The x-coordinate of the target relative from the robot in
+         * meter.
+         * @param y The y-coordinage of the target relative from the robot in
+         * meter.
+         */
         virtual void newTargetDetected(const float x, const float y) = 0;
     };
 
-    void registerNewTargetDetectedCallback(DetectionInterface* cb) {
-	detectionInterface = cb;
+    /**
+     * Registers the detection callback.
+     * @param cb Pointer to the callback interface.
+     */
+    void registerNewTargetDetectedCallback(DetectionInterface *cb) {
+        detectionInterface = cb;
     }
 
     void start();
     void stop();
 
     /**
-     * Needs to be registered with the LIDAR from main()
+     * Needs to be registered with the LIDAR from main().
+     * This is the LIDAR callback provding this class with fresh
+     * LIDAR data!
      */
     void newScanAvail(C1LidarData (&data)[C1Lidar::nDistance]);
 
     /**
-     * For debugging purposes and the GUI we can get the current left camera image
+     * For debugging purposes and the GUI we can get the current left camera
+     * image. This is pure polling but thread-safe.
      */
-    const cv::Mat getCurrentLCameraImage() const
-    {
+    const cv::Mat getCurrentLCameraImage() {
+        std::lock_guard<std::mutex> guard(leftImage_mutex);
         return currentL;
     }
 
     /**
-     * For debugging purposes and the GUI we can get the current right camera image
+     * For debugging purposes and the GUI we can get the current right camera
+     * image. This is pure polling but thread-safe.
      */
-    const cv::Mat getCurrentRCameraImage() const
-    {
+    const cv::Mat getCurrentRCameraImage() {
+        std::lock_guard<std::mutex> guard(rightImage_mutex);
         return currentR;
     }
 
     /**
-     * For debugging purposes and the GUI we can get the current disparity readings
+     * For debugging purposes and the GUI we can get the current disparity
+     * readings. This is pure polling but thread-safe.
      */
-    const cv::Mat getCurrentDisparityMap()
-    {
-	std::lock_guard<std::mutex> guard(disparityData_mutex);
+    const cv::Mat getCurrentDisparityMap() {
+        std::lock_guard<std::mutex> guard(disparityData_mutex);
         return currentD;
     }
 
-    const std::vector<cv::Point2f> getCurrentLidarCoords()
-    {
+    const std::vector<cv::Point2f> getCurrentLidarCoords() {
         std::lock_guard<std::mutex> guard(lidarData_mutex);
         return currentLidarCoords;
     }
 
     const std::vector<cv::Point> getQRcodeContour() {
         std::lock_guard<std::mutex> guard(contour_mutex);
-	return scaledContour;
+        return scaledContour;
     }
 
     static constexpr int LIDAR_DATA_POINTS = C1Lidar::nDistance;
@@ -78,16 +92,16 @@ public:
     /**
      * Factor which maps the x-pixel pos to meter
      **/
-    float xpos2meter = 1.0/400.0;
+    float xpos2meter = 1.0 / 400.0;
 
-private:
+  private:
     cv::Mat currentL;
     cv::Mat currentR;
     cv::Mat currentD;
 
     const char *LIDAR_SERIAL_DEV = "/dev/ttyAMA0";
 
-    void onTargetDetected(const std::vector<cv::Point2f>& coords);
+    void onTargetDetected(const std::vector<cv::Point2f> &coords);
 
     Libcam2OpenCV cameraL;
     Libcam2OpenCV cameraR;
@@ -115,5 +129,5 @@ private:
 
     std::vector<cv::Point> scaledContour;
 
-    DetectionInterface* detectionInterface = nullptr;
+    DetectionInterface *detectionInterface = nullptr;
 };
