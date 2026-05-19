90c78dc7d0fc2850ca7e17f3f5d507a5873d3c68
glafratta <glafratta@outlook.com>
2026-05-19 14:31:23 +0100

    moved comments to assess wheel speeds

--- custom_robot.h
+++ custom_robot.h
@@ -68,7 +68,6 @@ void getData(const Task::Action &a)override{
 	otherStuff();
     setRightWheelSpeed(R); //temporary fix because motors on despacito are the wrong way around
     setLeftWheelSpeed(L);
-	printf(",R=%f\tL=%f\n",R, L);
 }
 
 /**
@@ -112,21 +111,21 @@ class OpenLooper: public DeadReckoner, public MotorCallback{
     }
 
     bool hasTaskEnded(Task & t)override{
-		std::cout<<"Open loop checking task has ended ="<<motorStep<=0 <<std::endl;
+		//std::cout<<"Open loop checking task has ended ="<<int(motorStep<=0) <<std::endl;
         return motorStep<=0;
     }
 
-	// void otherStuff()override{
-	// 	// //std::cout<<"Open looping!"<<std::endl;
-    //     // if (L!=0 && R!=0){
-    //     //     motorStep--;
-    //     // }
-    //     // if (motorStep==0){
-    //     //     L=0;
-    //     //     R=0;
-    //     // }
-	// 	// std::cout<<"Motor step "<<motorStep<<std::endl;
-	// }
+	void otherStuff()override{
+		//std::cout<<"Open looping!"<<std::endl;
+        // if (L!=0 && R!=0){
+        //     motorStep--;
+        // }
+        if (motorStep==0){
+            L=0;
+            R=0;
+        }
+		// std::cout<<"Motor step "<<motorStep<<std::endl;
+	}
 
 	void on_new_reading(const Task &task, const Task & goal){
         if (L!=0 && R!=0){
@@ -137,6 +136,8 @@ class OpenLooper: public DeadReckoner, public MotorCallback{
             R=0;
         }
 		std::cout<<"Motor step "<<motorStep<<std::endl;
+		printf("R=%f\tL=%f\n",R, L);
+
     }
 
 };
