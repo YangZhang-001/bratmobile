9b519fe8901d92a33f2800533650cecac35bd31c
glafratta <glafratta@outlook.com>
2026-05-19 14:57:36 +0100

    max omega instead of speed to calculate the wheel speed for turning!

--- custom_robot.h
+++ custom_robot.h
@@ -116,10 +116,10 @@ class OpenLooper: public DeadReckoner, public MotorCallback{
     }
 
 	void otherStuff()override{
-		//std::cout<<"Open looping!"<<std::endl;
-        // if (L!=0 && R!=0){
-        //     motorStep--;
-        // }
+		std::cout<<"Open looping!"<<std::endl;
+        if (L!=0 && R!=0){
+            motorStep--;
+        }
         if (motorStep==0){
             L=0;
             R=0;
